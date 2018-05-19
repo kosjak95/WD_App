a3ad521a5753e22bd1bad332aec04ce0e5cbcbb1
kosjak95 <38251276+kosjak95@users.noreply.github.com>
2018-05-19 15:28:39 +0200

    Needed activities
    Basic activities we were talking about

--- app/src/main/java/jakubkosman/wd_app/LoginActivity.java
+++ app/src/main/java/jakubkosman/wd_app/LoginActivity.java
@@ -3,6 +3,7 @@ package jakubkosman.wd_app;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +29,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -63,6 +65,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mLoginFormView;
     private DatabaseHelper databaseHelper;
 
+    private String indeks = "128629";
+    private String pesel = "12345678900";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -95,7 +100,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mProgressView = findViewById(R.id.login_progress);
 
         databaseHelper = new DatabaseHelper(this);
-        databaseHelper.insertStudent();
+        //databaseHelper.insertStudent();
     }
 
     private void populateAutoComplete() {
@@ -157,24 +162,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
-        String index = mIndexView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        String temp_index = mIndexView.getText().toString();
+        String temp_password = mPasswordView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        if (!TextUtils.isEmpty(temp_password) && !isPasswordValid(temp_password)) {
             focusView = mPasswordView;
             cancel = true;
         }
 
         // Check for a valid email address.
-        if (TextUtils.isEmpty(index)) {
+        if (TextUtils.isEmpty(temp_index)) {
             mIndexView.setError(getString(R.string.error_field_required));
             focusView = mIndexView;
             cancel = true;
-        } else if (!isIndexValid(index)) {
+        } else if (!isIndexValid(temp_index)) {
             mIndexView.setError(getString(R.string.error_invalid_index));
             focusView = mIndexView;
             cancel = true;
@@ -187,10 +192,23 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(index, password);
-            mAuthTask.execute((Void) null);
+            if(temp_index.equals(indeks) && temp_password.equals(pesel)) {
+                showProgress(true);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+            else
+                Toast.makeText(getApplicationContext(), "Podano błędne dane logowania", Toast.LENGTH_LONG).show();
+//            showProgress(false);
+//            mAuthTask = new UserLoginTask(index, password);
+//            mAuthTask.execute((Void) null);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showProgress(false);
     }
 
     private boolean isIndexValid(String index) {
@@ -200,7 +218,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isPasswordValid(String password) {
 
-        if(password.length() >= 8) {
+        if(password.length() != 11) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             return false;
         }
--- app/src/main/java/jakubkosman/wd_app/MainActivity.java
+++ app/src/main/java/jakubkosman/wd_app/MainActivity.java
@@ -1,8 +1,7 @@
 package jakubkosman.wd_app;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,14 +17,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +39,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view)
+    {
+        Intent intent = null;
+        switch (view.getId())
+        {
+            case R.id.button_join:
+                intent = new Intent(MainActivity.this, JoinGroupActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button_view:
+                intent = new Intent(MainActivity.this, ViewGroupsActivity.class);
+                startActivity(intent);
+                break;
+            
+        }
+
     }
 }
